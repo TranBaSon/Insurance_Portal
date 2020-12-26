752dbfb9e12ea2d56f3465a2b44c4339f21ef7d6
TranBaSon <sontbth1808021@fpt.edu.vn>
2020-12-27 02:31:25 +0700

    fix deploy

--- target/generated-sources/annotations/com/gen/com/Insurance_portal/common/mappers/UserMapperImpl.java
+++ target/generated-sources/annotations/com/gen/com/Insurance_portal/common/mappers/UserMapperImpl.java
@@ -7,7 +7,7 @@ import javax.annotation.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-12-27T02:10:21+0700",
+    date = "2020-12-27T02:29:45+0700",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 1.8.0_265 (AdoptOpenJDK)"
 )
 public class UserMapperImpl implements UserMapper {
